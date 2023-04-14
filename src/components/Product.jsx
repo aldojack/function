f13c2b8f02a85ec276084b80d103e5519863ad00
Alan Jack <60137518+aldojack@users.noreply.github.com>
2023-04-14 11:20:43 +0100

    Dynamic product component added

---
+++ src/components/Product.jsx
@@ -0,0 +1,19 @@
+import placeholder from "../assets/product-image-1.png";
+import Heart from "../assets/heart.svg";
+
+export default function Product({img, name, price}) {
+  return (
+    <div className="max-w-[253px] md:max-w-[332px] shrink-0">
+      <img src={img} />
+      <div className="flex justify-between items-center mt-6">
+        <div className="flex flex-col">
+          <p>{name}</p>
+          <p>{price}</p>
+        </div>
+        <div>
+          <img src={Heart} />
+        </div>
+      </div>
+    </div>
+  );
+}
