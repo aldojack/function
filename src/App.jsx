60aeddf2a9b958ffb2f40bd24bfd9162979e8618
Alan Jack <60137518+aldojack@users.noreply.github.com>
2023-04-14 20:18:21 +0100

    Added footer and fixed styling of main

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,22 @@ import Banner from "./components/Banner"
 import CarouselContainer from "./components/CarouselContainer"
 import DoorwayContainer from "./components/DoorwayContainer"
 import Feature from "./components/Feature"
+import Footer from "./components/Footer"
 import Navbar from "./components/Navbar"
 
 function App() {
   
 
   return (
-    <div className="w-full">
+    <div className="w-full min-h-screen flex flex-col">
       <Navbar/>
-      <main className="h-screen">
+      <main className="flex-grow">
       <Feature/>
       <CarouselContainer/>
       <DoorwayContainer/>
       <Banner/>
       </main>
+      <Footer/>
     </div>
   )
 }
